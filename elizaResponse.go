d68a267fbfbd85ca51f82bb8159d21556b12d3ed
Kevin Barry <g00339811@gmit.ie>
2017-11-15 20:16:06 +0000

    3. Capture “I am “

--- elizaResponse.go
+++ elizaResponse.go
@@ -29,18 +29,24 @@ func elizaResponse(inputStr string) string {
 	*/
 	input := inputStr
 	pattern := `.*father.*`
+	pattern2 := `I am (.*)`
 	output := "why dont you tell me more about your father?"
 	response := ""
 	rand.Seed(time.Now().UTC().UnixNano())
+
 	answers := []string{
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
 	}
 
-	//check if pattern is fouund in string
+	re := regexp.MustCompile(pattern2)
+	//check if pattern is fouund in string (father/I am)
 	if matched, _ := regexp.MatchString(pattern, input); matched {
-		response = "input :" + input + " \noutput :" + output
+		response = "input :" + input + "\noutput :" + output
+	} else if re.MatchString(input) {
+		match := re.ReplaceAllString(input, "How do you know you are $1?")
+		response = "input :" + input + " \noutput : " + match
 	} else {
 		response = "input :" + input + " \noutput :" + answers[rand.Intn(len(answers))]
 	}
@@ -49,16 +55,21 @@ func elizaResponse(inputStr string) string {
 
 func main() {
 	//slice of inputs
+
 	userInput := []string{
 		"People say I look like both my mother and father.",
 		"Father was a teacher.",
 		"I was my father’s favourite.",
 		"I'm looking forward to the weekend.",
 		"My grandfather was French!",
+		"I am happy.",
+		"I am not happy with your responses.",
+		"I am not sure that you understand the effect that your questions are having on me.",
+		"I am supposed to just take what you’re saying at face value?",
 	}
+
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	word := elizaResponse(userInput[rand.Intn(len(userInput))])
-
+	word := elizaResponse(userInput[rand.Intn(len("userInput"))])
 	fmt.Print(word)
 }
