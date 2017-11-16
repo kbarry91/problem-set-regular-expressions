c539b55750ae590854042238842f2f5dbf1d7e87
Kevin Barry <g00339811@gmit.ie>
2017-11-16 18:00:35 +0000

    5. Reflect pronouns

--- elizaResponse.go
+++ elizaResponse.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math/rand"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -29,7 +30,6 @@ func elizaResponse(inputStr string) string {
 	*/
 	input := inputStr
 	pattern := `.*father.*`
-	pattern2 := []string{`.*i am(.*)`, `.*I AM(.*)`, `.*I'm(.*)`, `.*i'm(.*)`, `.*im(.*)`, `.*I am(.*)`}
 	output := "why dont you tell me more about your father?"
 	response := ""
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -43,29 +43,58 @@ func elizaResponse(inputStr string) string {
 	//check if pattern is fouund in string (father/I am)
 	if matched, _ := regexp.MatchString(pattern, input); matched {
 		response = "input :" + input + "\noutput :" + output
-		//} //else if re.MatchString(input) {
-		//match := re.ReplaceAllString(input, "How do you know you are $1?")
-		//response = "input :" + input + " \noutput : " + match
+
 	} else {
 		response = "input :" + input + " \noutput :" + answers[rand.Intn(len(answers))]
 	}
 
-	// loop through pattern2 array
-	//if pattern is found extract substring
-	//set response
-	for _, checkPattern := range pattern2 {
-		re := regexp.MustCompile(checkPattern)
-		if re.MatchString(input) {
-			match := re.ReplaceAllString(input, "How do you know you are $1?")
-			response = "input :" + input + " \noutput : " + match
-			return response
-
-		} //if re.MatchString
-	} //for pattern2
-
+	//Check if input contains I am or prounouns
+	//assign pattern to re
+	re := regexp.MustCompile("(?i)(i[' a]*m) (.*)")
+	if re.MatchString(input) {
+		//capture subtring array of input after a match is found
+		matched := re.FindStringSubmatch(input)[2]
+		//pass matched string array to matchPronouns to reflect pronouns
+		matched = matchPronouns(matched)
+		response = "input :" + input + " \noutput : How do you know you are " + matched
+		return response
+	}
 	return response
 } //elizaResponse
 
+func matchPronouns(inputStr string) string {
+	// split inputStr into slice of strings
+	splitStr := strings.Fields(inputStr)
+
+	//map of reflected pronouns
+	pronouns := map[string]string{
+		"i":      "you",
+		"was":    "were",
+		"i'd":    "you would",
+		"i've":   "you have",
+		"i'll":   "you will",
+		"my":     "your",
+		"are":    "am",
+		"you've": "I have",
+		"you'll": "I will",
+		"your":   "my",
+		"yours":  "mine",
+		"you":    "I",
+		"me":     "you",
+		"me.":    "you",
+		"you're": "I’m",
+	}
+
+	//loop map word to check if splitStr is a pronoun
+	// Found how to iterate over a map from https://gobyexample.com/range
+	for index, word := range splitStr {
+		if value, ok := pronouns[strings.ToLower(word)]; ok {
+			splitStr[index] = value
+		}
+	}
+
+	return strings.Join(splitStr, " ")
+}
 func main() {
 	//slice of inputs
 
@@ -75,7 +104,6 @@ func main() {
 		"I was my father’s favourite.",
 		"I'm looking forward to the weekend.",
 		"My grandfather was French!",
-		"I am happy.",
 		"I am not happy with your responses.",
 		"I am not sure that you understand the effect that your questions are having on me.",
 		"I am supposed to just take what you’re saying at face value?",
@@ -84,5 +112,7 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	word := elizaResponse(userInput[rand.Intn(len("userInput"))])
+	fmt.Print("\n")
 	fmt.Print(word)
+	fmt.Print("\n")
 }
