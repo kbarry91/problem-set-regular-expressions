8a4c783d421f70669f68e6a4fba5b9688d383a91
Kevin Barry <g00339811@gmit.ie>
2017-12-01 00:12:49 +0000

    added more responses and option to enter user input

--- elizaResponse.go
+++ elizaResponse.go
@@ -59,6 +59,41 @@ func elizaResponse(inputStr string) string {
 		response = "input :" + input + " \noutput : How do you know you are " + matched
 		return response
 	}
+	//Check if input contains "i xxxx it"
+	//assign pattern to re
+	re = regexp.MustCompile("(?:i(.*)it)")
+	if re.MatchString(input) {
+		//capture subtring array of input after a match is found
+		matched := re.FindStringSubmatch(input)[1]
+		//pass matched string array to matchPronouns to reflect pronouns
+		matched = matchPronouns(matched)
+		response = "input :" + input + " \noutput : Why do you " + matched + " it? "
+		return response
+	}
+
+	//Check if input contains "i xxxx you"
+	//assign pattern to re
+	re = regexp.MustCompile("(?:i(.*)you)")
+	if re.MatchString(input) {
+		//capture subtring array of input after a match is found
+		matched := re.FindStringSubmatch(input)[1]
+		//pass matched string array to matchPronouns to reflect pronouns
+		matched = matchPronouns(matched)
+		response = "input :" + input + " \noutput : Why do you  " + matched + " me?"
+		return response
+	}
+
+	//Check if input contains a work reference
+	//assign pattern to re
+	re = regexp.MustCompile("(?:work|job|employ|working) (.*)")
+	if re.MatchString(input) {
+		//capture subtring array of input after a match is found
+		matched := re.FindStringSubmatch(input)[1]
+		//pass matched string array to matchPronouns to reflect pronouns
+		matched = matchPronouns(matched)
+		response = "input :" + input + " \noutput :And do you enjoy workin " + matched + "?"
+		return response
+	}
 	return response
 } //elizaResponse
 
@@ -96,8 +131,8 @@ func matchPronouns(inputStr string) string {
 	return strings.Join(splitStr, " ")
 }
 func main() {
+	// **********************Automatically generated input *******************************
 	//slice of inputs
-
 	userInput := []string{
 		"People say I look like both my mother and father.",
 		"Father was a teacher.",
@@ -108,11 +143,25 @@ func main() {
 		"I am not sure that you understand the effect that your questions are having on me.",
 		"I am supposed to just take what you’re saying at face value?",
 	}
-
 	rand.Seed(time.Now().UTC().UnixNano())
-
+	//word := elizaResponse("")
 	word := elizaResponse(userInput[rand.Intn(len("userInput"))])
 	fmt.Print("\n")
 	fmt.Print(word)
 	fmt.Print("\n")
+	/*
+		// **********************Manually user entered input *******************************
+		//uncomment the following section for userInput from console.You must comment out lines 136-151
+
+		for reader := bufio.NewReader(os.Stdin); ; {
+			fmt.Print("Please enter a query to ask eliza!")
+			userInput, _ := reader.ReadString('\n')
+			userInput = strings.Trim(userInput, "\r\n")
+			word := elizaResponse(userInput)
+			fmt.Print("\n")
+			fmt.Print(word)
+			fmt.Print("\n")
+		}
+	*/
+
 }
